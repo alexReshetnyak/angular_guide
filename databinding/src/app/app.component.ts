39211f89fc6cca77ac398f516e21b1f48b96af85
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-11-12 10:05:33 +0200

    added minor tweaks to app component

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+  public serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
   public onServerAdded(serverData: {serverName: string, serverContent: string}): void {
     this.serverElements.push({
