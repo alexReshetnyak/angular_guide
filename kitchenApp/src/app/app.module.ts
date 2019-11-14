8c8fd14a42c38af472fdaf94b5704b5aee534279
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-11-14 10:39:03 +0200

    added small tweaks to server-element component

--- kitchenApp/src/app/app.module.ts
+++ kitchenApp/src/app/app.module.ts
@@ -1,3 +1,5 @@
+// * Augury - add for chrome to debug application
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
