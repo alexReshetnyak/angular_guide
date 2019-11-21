81f3b04760db3c1233ed2d3246d25d33f7885f68
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-11-21 10:37:39 +0200

    worked on navbar menu

--- kitchenApp/src/app/shared/directives/dropdown/dropdown.directive.ts
+++ kitchenApp/src/app/shared/directives/dropdown/dropdown.directive.ts
@@ -1,12 +1,30 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import {
+  Directive,
+  HostListener,
+  HostBinding,
+  Input,
+  ElementRef,
+  Renderer2
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.show') isOpen = false;
+  // @HostBinding('class.show') isOpen = false;
+  @Input() cssClass = 'show';
+  private isOpen = false;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('click') toggleOpen() {
+    console.log('#Dropdown directive cssClass', this.cssClass);
+
     this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      this.renderer.addClass(this.elRef.nativeElement, this.cssClass);
+    } else {
+      this.renderer.removeClass(this.elRef.nativeElement, this.cssClass);
+    }
   }
 }
