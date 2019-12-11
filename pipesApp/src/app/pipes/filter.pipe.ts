eff2f6234cda8b9781a96ee3dbae3d00ac4022fc
AReshetnyak <alexreshetnyakmail@gmail.com>
2019-12-11 10:37:58 +0200

    added Pipe app

---
+++ pipesApp/src/app/pipes/filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  pure: false
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(values: any[], filterString: string, propName: string): any {
+    if (values.length === 0 || filterString === '') { return values; }
+    const filteredArray = values.filter(value => value[propName] === filterString);
+    return filteredArray;
+  }
+
+}
